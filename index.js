be3cd4be3e820b67f758dd45c0caa57be1f81e66
Exequiel Lamagni <exequiellamagni99@gmail.com>
2024-01-03 11:35:00 -0300

    solicitud post, con manejo de errores (exc 3.5 y 3.6)

--- index.js
+++ index.js
@@ -58,6 +58,44 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
+app.post( '/api/persons' ,(request,response)=>{
+    const body = request.body
+
+    const generateId = () => {
+        const max = 1000
+        const min = 1
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    if(!body.name){
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if(!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if(persons.some(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+
+})
+
 const PORT = 3001
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
