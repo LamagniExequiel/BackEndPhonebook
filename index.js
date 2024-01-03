7bb531bd6e845632f4add4955bc7e33664b2447f
Exequiel Lamagni <exequiellamagni99@gmail.com>
2024-01-03 13:43:34 -0300

    registro de mensajes con morgan(exc 3.7 y 3.8)

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+morgan.token('body',(req,res,method) => {
+    if(method === 'POST'){
+        return JSON.stringify(req.body)
+    }
+    return null
+})
+
 app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.body(req,res,tokens.method(req,res))
+    ].join(' ')
+  }))
+
 
 let persons = [
     {
